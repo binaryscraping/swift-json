3c42825ccb16c994d9f083eb9249571a107b59a3
Guilherme Souza <grsouza@pm.me>
2021-08-19 06:28:59 -0300

    Add some helpers

--- Sources/JSON/JSON.swift
+++ Sources/JSON/JSON.swift
@@ -36,6 +36,19 @@ public enum JSON: Hashable {
     }
   }
 
+  public var numberValue: Double? {
+    get {
+      switch self {
+      case .number(let value): return value
+      default: return nil
+      }
+    }
+
+    set {
+      self = newValue.map(JSON.number) ?? .null
+    }
+  }
+
   public var objectValue: [String: JSON]? {
     get {
       if case .object(let object) = self {
@@ -183,5 +196,88 @@ extension JSON {
     } catch {
       return ""
     }
+  }
+}
+
+extension JSON {
+  public func codableValue<T: Codable>() throws -> T {
+    let data = try JSONEncoder().encode(self)
+    return try JSONDecoder().decode(T.self, from: data)
+  }
+
+  public init(data: Data, decoder: JSONDecoder = JSONDecoder()) throws {
+    self = try decoder.decode(JSON.self, from: data)
+  }
+
+  public func asData(encoder: JSONEncoder = JSONEncoder()) throws -> Data {
+    try encoder.encode(self)
+  }
+
+  private enum JSONError: Error {
+    case unknown
+    case nonJSONType(type: String)
+  }
+
+  public init(value: Any) throws {
+    switch value {
+    case _ as NSNull:
+      self = .null
+    case let number as NSNumber:
+      if number.isBool() {
+        self = .bool(number.boolValue)
+      } else {
+        self = .number(number.doubleValue)
+      }
+
+    case nil:
+      self = .null
+    case let url as URL:
+      self = .string(url.absoluteString)
+    case let string as String:
+      self = .string(string)
+    case let bool as Bool:
+      self = .bool(bool)
+    case let array as [Any]:
+      self = .array(try array.map(JSON.init))
+    case let object as [String: Any]:
+      self = .object(try object.mapValues(JSON.init))
+    default:
+      throw JSONError.nonJSONType(type: "\(value.self)")
+    }
+  }
+
+  public var rawValue: Any {
+    switch self {
+    case let .array(array):
+      return array.map(\.rawValue)
+    case let .object(object):
+      return object.mapValues(\.rawValue)
+    case let .bool(bool):
+      return bool
+    case .null:
+      return NSNull()
+    case let .number(double):
+      return double
+    case let .string(string):
+      return string
+    }
+  }
+}
+
+// MARK: - Helpers
+extension NSNumber {
+  fileprivate static let trueValue = NSNumber(value: true)
+  fileprivate static let trueObjCType = trueValue.objCType
+  fileprivate static let falseValue = NSNumber(value: false)
+  fileprivate static let falseObjCType = falseValue.objCType
+
+  fileprivate func isBool() -> Bool {
+    let type = self.objCType
+    if (compare(NSNumber.trueValue) == .orderedSame && type == NSNumber.trueObjCType)
+      || (compare(NSNumber.falseValue) == .orderedSame && type == NSNumber.falseObjCType)
+    {
+      return true
+    }
+    return false
   }
 }
